29886ca97295310591df45576555bd1ea605012b
logicappsource <logicappsource@gmail.com>
2019-05-23 18:17:53 +0200

    Bruger UI
    Burger UI Interface

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 
-const burgerIngredient = (props) => {
+class BurgerIngredient extends Component {
+  render() {
     let ingredient = null;
 
-    switch (props.type){
+    switch (this.props.type){
       case('bread-bottom'):
           ingredient = <div className={classes.BreadBottom}></div>;
           break;
       case('bread-top'):
-          ingredient = (
-            <div className={classes.BreadTop}>
-              <div className={classes.Seeds1}></div>
-              <div className={classes.Seeds2}></div>
-            </div>
-          );
-          break;
+          ingredient = <div className={classes.BreadTop}></div>;
       case('meat'):
-          ingredient = <div className={classes.Meat}></div>
+          ingredient = <div className={classes.Meat}></div>;
+          break;
+      case('cheese'):
+          ingredient = <div className={classes.Cheese}></div>;
+          break;
+      case('bacon'):
+          ingredient = <div className={classes.Bacon}></div>;
+          break;
+      case('salad'):
+          ingredient = <div className={classes.Bacon}></div>;
+          break;
+      default:
+          ingredient = null;
     }
-};
+    return ingredient;
+  }
+}
 
-export default burgerIngredient;
+BurgerIngredient.PropTypes = {
+  type: PropTypes.string.isRequired
+}
+
+export default BurgerIngredient;
