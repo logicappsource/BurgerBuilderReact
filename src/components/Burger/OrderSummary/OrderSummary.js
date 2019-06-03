7af796664ce91f32bd313a7ff18d40b9b0695089
logicappsource <logicappsource@gmail.com>
2019-06-03 18:27:59 +0200

    Sidedrawer
    - Sidedrawer
    - Responsive Mobile
    - Backdrop
    - Reorganize folder structure
    
     MAJOR UPDATE

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component{
 
-  const ingrdientsSummary = Object.keys(props.ingredients)
-      .map(igKey => {
-        return (
-          <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
-        </li>);
-      })
+  componentWillUdpdate() {
+    console.log('Order sumamry wil update')
+  }
 
-  return (
-       <Aux>
-          <h3> Your Order </h3>
-            <p> Delicous Burger with the following ingredients</p>
-          <ul>
-              {ingrdientsSummary}
-          </ul>
-          <p><strong>Total Price: {props.price.toFixed(2)} </strong> </p>
-          <p> Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchasedCancelled}>Cancell</Button>
-            <Button btnType="Success" clicked={props.purchasedContinued}>Continue</Button>
-      </Aux>
-  )
-};
+  render() {
 
-export default orderSummary;
+    const ingrdientsSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+          return (
+            <li key={igKey}>
+              <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
+          </li>);
+        })
+
+    return (
+      <Aux>
+         <h3> Your Order </h3>
+           <p> Delicous Burger with the following ingredients</p>
+         <ul>
+             {ingrdientsSummary}
+         </ul>
+         <p><strong>Total Price: {this.props.price.toFixed(2)} </strong> </p>
+         <p> Continue to Checkout?</p>
+           <Button btnType="Danger" clicked={this.props.purchasedCancelled}>Cancell</Button>
+           <Button btnType="Success" clicked={this.props.purchasedContinued}>Continue</Button>
+     </Aux>
+   );
+  }
+}
+
+
+export default OrderSummary;
--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './logo.css';
 
 const logo = (props) => (
-  <div className={classes.Logo}><img src={burgerLogo} alt="BurgerBuilder" /></div>
+  <div className={classes.Logo}><img src={burgerLogo} alt="BurgerBuilder" style={{height: props.heiight}} /></div>
 );
 
 export default logo;
