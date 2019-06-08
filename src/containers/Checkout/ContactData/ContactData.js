36a4965955bc99a040747ea4a5d60d2386d04044
logicappsource <logicappsource@gmail.com>
2019-06-08 20:12:02 +0200

    Element Input Dynamicaly rendered
    Element Input Dynamicaly rendered

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+         name: {
+           elementType: 'input',
+           elementConfig: {
+             type: 'text',
+             placeholder: 'Your Name'
+           },
+           value: ''
+         },
+          street: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'Street'
+            },
+            value: ''
+          },
+          zipCode: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'Zipcode'
+            },
+            value: ''
+          },
+          country: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'Country'
+            },
+            value: ''
+          },
+          email: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'Your Email'
+            },
+            value: ''
+          },
+          deliveryMethod: {
+            elementType: 'select',
+            elementConfig: {
+              options: [
+                {value: 'fastest', displayValue: 'Fastest'},
+                {value: 'cheapest', displayValue: 'Cheapest'}
+              ]
+            },
+            value: ''
+          }
     },
     loading: false
   }
@@ -22,16 +68,6 @@ orderHandler = (event) => {
       const order = {
         ingredients: this.props.ingredients,
         price: this.props.price,
-        customer: {
-          name: 'Patrick',
-          address: {
-            street: 'sdsdsdsd',
-            zipCode: '23131',
-            country: 'denmark'
-          },
-          email: 'test@test.com'
-        },
-        deliveryMethod: 'fastest'
       }
 
       axios.post( '/orders.json', order)
@@ -46,12 +82,24 @@ orderHandler = (event) => {
 }
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+        formElementsArray.push({
+          id: key,
+          config: this.state.orderForm[key]
+        });
+    }
+
     let form = (
       <form>
-          <Input inputtype="input" type="text" name="name" placeholder="your Name" />
-          <Input inputtype="input" type="text" name="email" placeholder="your Email"/>
-          <Input inputtype="input" type="text" name="Street" placeholder="your Street" />
-          <Input inputtype="input" type="text" name="postalcode" placeholder="your Postal Code" />
+          {formElementsArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+            />
+          ))}
           <Button btntype="Success" clicked={this.orderHandler}>ORDER </Button>
       </form>
     );
