2aab030ef9daf71fcce60f4075ecd5eec6d6cb80
logicappsource <logicappsource@gmail.com>
2019-06-08 15:55:47 +0200

    Adding Order Page
    Adding Order page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from'./containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends React.Component {
   state = {
@@ -15,6 +16,7 @@ class App extends React.Component {
           <Layout>
              <Switch>
               <Route path="/checkout" component={Checkout} />
+              <Route path="/orders" exact component={Orders} />
               <Route path="/" exact component={BurgerBuilder} />
             </Switch>
           </Layout>
