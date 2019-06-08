f9bf72081b39eba067ec20e2bf6e5fa1dac11b59
logicappsource <logicappsource@gmail.com>
2019-06-08 15:47:14 +0200

    Order Submission - passing data between checkout
    Order Submission - passing data between checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,37 +88,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-/*
-    this.setState({loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Patrick',
-        address: {
-          street: 'sdsdsdsd',
-          zipCode: '23131',
-          country: 'denmark'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-
-      .then(response => {
-        this.setState({ loading: false, purchasing: false })
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false })
-        console.log(error)
-      })
-      */
-
       const queryParams = [];
       for (let i in this.state.ingredients) {
           queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
       }
+      queryParams.push('price=', this.state.totalPrice);
       const queryString = queryParams.join('&');
       this.props.history.push({
           pathname: '/checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+
 
 class ContactData extends Component {
   state = {
@@ -9,25 +12,56 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
-    }
+    },
+    loading: false
   }
 
-  componentDidMount(){
-    console.log('contactdata')
-  }
+orderHandler = (event) => {
+  event.preventDefault();
+      this.setState({loading: true });
+      const order = {
+        ingredients: this.props.ingredients,
+        price: this.props.price,
+        customer: {
+          name: 'Patrick',
+          address: {
+            street: 'sdsdsdsd',
+            zipCode: '23131',
+            country: 'denmark'
+          },
+          email: 'test@test.com'
+        },
+        deliveryMethod: 'fastest'
+      }
+      axios.post('/orders.json', order)
+        .then(response => {
+          this.setState({ loading: false})
+          this.props.history.push('/')
+        })
+        .catch(error => {
+          this.setState({ loading: false })
+          console.log(error)
+        })
+}
 
   render() {
-    console.log('ddd')
+    let form = (
+      <form>
+          <input className={classes.Input} type="text" name="name" placeholder="your Name" />
+          <input className={classes.Input} type="text" name="email" placeholder="your Email"/>
+          <input className={classes.Input} type="text" name="Street" placeholder="your Street" />
+          <input className={classes.Input} type="text" name="postalcode" placeholder="your Postal Code" />
+          <Button btnType="Success" clicked={this.orderHandler}>ORDER </Button>
+      </form>
+    );
+    if(this.state.loading) {
+      form = <Spinner />;
+    }
+
     return(
         <div className={classes.ContactData}>
             <h4>Enter your Contact Data</h4>
-            <form>
-                <input type="text" name="name" placeholder="your Name" />
-                <input type="text" name="email" placeholder="your Email"/>
-                <input type="text" name="Street" placeholder="your Street" />
-                <input type="text" name="postalcode" placeholder="your Postal Code" />
-                <Button btnType="Success">ORDER </Button>
-            </form>
+            {form}
         </div>
     );
   }
