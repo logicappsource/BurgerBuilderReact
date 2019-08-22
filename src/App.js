b7a741177799517c3499b6a33fde6de213f8a130
logicappsource <logicappsource@gmail.com>
2019-08-22 18:14:18 +0200

    Auth form
    Auth form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from'./containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends React.Component {
   state = {
@@ -17,6 +18,7 @@ class App extends React.Component {
              <Switch>
               <Route path="/checkout" component={Checkout} />
               <Route path="/orders" exact component={Orders} />
+              <Route path="/auth" exact component={Auth} />
               <Route path="/" exact component={BurgerBuilder} />
             </Switch>
           </Layout>
