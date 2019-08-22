43f2788ac213727474a0662a54329fad36ad71cf
logicappsource <logicappsource@gmail.com>
2019-08-22 20:13:42 +0200

    Accessign protected ressources
    Accessign protected ressources

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -105,7 +105,7 @@ orderHandler = (event) => {
         orderData: formData
       }
 
-      this.props.onOrderBurger(order);
+      this.props.onOrderBurger(order, this.props.token);
 }
 
 checkValidity(value, rules) {
@@ -144,7 +144,6 @@ inputChangedHandler = (event, inputIdentifier) => {
     for (let inputIdentifier in updatedOrderForm) {
       formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
     }
-    console.log(updatedFormElement);
     this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
 }
 
@@ -192,13 +191,14 @@ const mapStateToProps = state => {
   return {
       ings: state.burgerBuilder.ingredients,
       price: state.burgerBuilder.totalPrice,
-      loading: state.order.loading
+      loading: state.order.loading,
+      token: state.auth.token
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-      onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+      onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
   };
 };
 
