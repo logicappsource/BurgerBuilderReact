5da8a39e6f1af439f8db4cf993f3de6a7d13f027
logicappsource <logicappsource@gmail.com>
2019-08-22 20:23:15 +0200

    Update UI on Dep. state
    if user is authenticated or not

--- src/components/navigation/NavigationItems/NavigationItems.js
+++ src/components/navigation/NavigationItems/NavigationItems.js
@@ -2,11 +2,14 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
      <ul className={classes.NavigationItems}>
         <NavigationItem exact link="/">Burger Builder </NavigationItem>
         <NavigationItem link="/orders">Orders </NavigationItem>
-        <NavigationItem link="/auth">Auth </NavigationItem>
+        { props.isAuthenticated
+          ? <NavigationItem link="/logout">Logout </NavigationItem>
+          : <NavigationItem link="/auth">Login </NavigationItem>
+        }
      </ul>
 );
 
