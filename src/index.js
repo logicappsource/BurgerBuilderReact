b58d4e16f97d5197e25c262c9c2bae34b8959007
logicappsource <logicappsource@gmail.com>
2019-08-22 13:04:30 +0200

    install redux dev tools
    install redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const logger = store => {
+    return next => {
+      return action => {
+        console.log('[Middleware] Dispatching', action);
+        const result = next(action);
+        console.log('[Middleware] next state', store.getState());
+        return result;
+      }
+    }
+};
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
   <Provider store={store}>
